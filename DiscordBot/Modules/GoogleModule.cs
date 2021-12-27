2f5e3c615fbe427703ee54c31358c8313e702c44
Tim De Gieter <timdegieter@gmail.com>
2021-12-27 22:28:20 +0100

    Add base idea

---
+++ DiscordBot/Modules/GoogleModule.cs
@@ -0,0 +1,25 @@
+﻿using Discord.Commands;
+using DiscordBot.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot.Modules
+{
+    public class GoogleModule : ModuleBase
+    {
+        public GoogleService GoogleService { get; set; }
+        public Settings.Deserialized.Settings Settings { get; set; }
+
+        [Command("google"), Alias("lmgtfy"), Priority(100)]
+        [Summary("Creates a google link with the query. Syntax: !google cute kittens")]
+        public async Task Google(string searchTerm)
+        {
+            string defaultLink = "https://lmgtfy.app/?q=";
+            searchTerm = searchTerm.Replace(" ", "+");
+            string link = defaultLink + searchTerm;
+        }
+    }
+}
---
+++ DiscordBot/Services/GoogleService.cs
@@ -0,0 +1,44 @@
+﻿using Discord.WebSocket;
+using DiscordBot.Services.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiscordBot.Services
+{
+    public class GoogleService
+    {
+        private readonly DiscordSocketClient client;
+        private readonly ILoggingService loggingService;
+        private bool isRunning;
+        public GoogleService(DiscordSocketClient client, ILoggingService loggingService, Settings.Deserialized.Settings settings)
+        {
+            this.client = client;
+            this.loggingService = loggingService;
+            isRunning = false;
+
+            client.Ready += OnReady;
+        }
+
+        private async Task OnReady()
+        {
+            if (!isRunning)
+            {
+                isRunning = true;
+                await loggingService.LogAction("Started LMGTFY Service");
+                await Listen();
+            }
+        }
+
+        private Task Listen()
+        {
+            while (isRunning)
+            {
+
+            }
+            return null;
+        }
+    }
+}
